c5584a9bbbaf3ac266bcd92cade2d6ab8c770b87
Dago R <dagoramirezserna@gmail.com>
2020-09-03 20:26:42 -0500

    Se crea el proyecto y se inicia con el consumo de la API

---
+++ main.js
@@ -0,0 +1,19 @@
+let pokemonDiv = document.querySelector('.pokemon');
+pokemonDiv.innerHTML = '';
+fetch('https://pokeapi.co/api/v2/pokemon/?limit=10')
+.then(response => response.json())
+.then(pokemon => {
+    pokemon.results.map(element => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${element.name}`)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result.sprites.front_default, element);
+            let imgPokemon = result.sprites.front_default;
+            console.log(result.sprites.front_default);
+            pokemonDiv.innerHTML += `<div class="col-4">
+                                        <img src="${result.sprites.front_default}" alt="imagen pokemon" class="img-fluid">
+                                        <h5>${element.name}</h5>
+                                    </div>`
+        });
+    });
+});
